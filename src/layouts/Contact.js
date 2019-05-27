e07a2f16c80d12205968904894a59c92f212561a
Lech Kulesza <lmkulesz@icloud.com>
2019-05-28 01:51:09 +0200

    form validation

--- src/layouts/Contact.js
+++ src/layouts/Contact.js
@@ -1,6 +1,47 @@
 import React, { Component } from "react";
 import "../style/Contact.sass";
 
+const isSpace = name => {
+  for (let i = 0; i < name.length; i++) {
+    if (name[i] === " ") return true;
+  }
+  return false;
+};
+
+const isMail = mail => {
+  let monkey = false;
+  let dot = false;
+  for (let i = 0; i < mail.length; i++) {
+    if (mail[i] === "@") monkey = true;
+    if (mail[i] === ".") dot = true;
+  }
+
+  if (monkey && dot) return true;
+  return false;
+};
+
+const formValidation = function(name, mail, topic, message) {
+  let flagName = false;
+  let flagMail = false;
+  let flagTopic = false;
+  let flagMessage = false;
+
+  if (name.length > 5 && isSpace(name)) flagName = true;
+  if (mail.length > 5 && isMail(mail)) flagMail = true;
+  if (topic.length > 0) flagTopic = true;
+  if (message.length > 10) flagMessage = true;
+  if (flagName && flagMail && flagTopic && flagMessage) {
+    return "clear";
+  } else {
+    const erroArr = [];
+    if (!flagName) erroArr.push("name");
+    if (!flagMail) erroArr.push("mail");
+    if (!flagTopic) erroArr.push("topic");
+    if (!flagMessage) erroArr.push("message");
+    return erroArr;
+  }
+};
+
 class Contact extends Component {
   state = {
     name: "",
@@ -11,30 +52,41 @@ class Contact extends Component {
 
   handleChangeName = e => {
     this.setState({
-      name: e.value
+      name: e.target.value
     });
   };
 
   handleChangeMail = e => {
     this.setState({
-      mail: e.value
+      mail: e.target.value
     });
   };
 
   handleChangeTopic = e => {
     this.setState({
-      topic: e.value
+      topic: e.target.value
     });
   };
 
   handleChangeMessage = e => {
     this.setState({
-      message: e.value
+      message: e.target.value
     });
   };
 
   handleClickForm = e => {
     e.preventDefault();
+    const error = formValidation(
+      this.state.name,
+      this.state.mail,
+      this.state.topic,
+      this.state.message
+    );
+    if (error === "clear") {
+      alert("bombastiko");
+    } else {
+      alert("ogarnij sie...");
+    }
   };
 
   render() {
