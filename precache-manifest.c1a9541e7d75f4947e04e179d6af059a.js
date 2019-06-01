90bc9b6a377db7a3e7142447bb1765b52810bad1
Lech Kulesza <lmkulesz@icloud.com>
2019-06-02 01:17:28 +0200

    Updates

---
+++ precache-manifest.c1a9541e7d75f4947e04e179d6af059a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0b725e7653ae7c0944e222c7bc031c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7bd2aab11090a24c1702",
+    "url": "/portfolio/static/css/main.2d6b7393.chunk.css"
+  },
+  {
+    "revision": "41b9863699e08563bc78",
+    "url": "/portfolio/static/js/2.560b5b3a.chunk.js"
+  },
+  {
+    "revision": "7bd2aab11090a24c1702",
+    "url": "/portfolio/static/js/main.1bf2e71f.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "149428251eb2119a8511b4e9d49f2e75",
+    "url": "/portfolio/static/media/baner.14942825.jpg"
+  },
+  {
+    "revision": "104f2ad991f91e91c51a9418c6ff65e2",
+    "url": "/portfolio/static/media/css3.104f2ad9.png"
+  },
+  {
+    "revision": "69efedfba46180f5e121885ef59b1866",
+    "url": "/portfolio/static/media/git.69efedfb.png"
+  },
+  {
+    "revision": "07e2ddc1f708c5e9bf6bc0065f54fa28",
+    "url": "/portfolio/static/media/jquery.07e2ddc1.png"
+  },
+  {
+    "revision": "edbc93052d5716d35a6f4731df6b342c",
+    "url": "/portfolio/static/media/me.edbc9305.jpg"
+  },
+  {
+    "revision": "8aea69c96e58d449edbc9cc9c82698c2",
+    "url": "/portfolio/static/media/node.8aea69c9.png"
+  },
+  {
+    "revision": "f56f06210684a937055c08af0d3effd8",
+    "url": "/portfolio/static/media/photoshop.f56f0621.png"
+  }
+]);
